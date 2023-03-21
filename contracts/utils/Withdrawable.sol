158f425e349ebc799a1775fea41d204b9abd341a
RJGonzalez40 <66503358+RJGonzalez40@users.noreply.github.com>
2023-03-21 00:03:18 -0400

    Update Withdrawable.sol

--- contracts/utils/Withdrawable.sol
+++ contracts/utils/Withdrawable.sol
@@ -1,14 +1,15 @@
-pragma solidity ^0.6.6;
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
     Ensures that any contract that inherits from this contract is able to
     withdraw funds that are accidentally received or stuck.
  */
- 
+
 contract Withdrawable is Ownable {
     using SafeERC20 for ERC20;
     address constant ETHER = address(0);
@@ -26,9 +27,8 @@ contract Withdrawable is Ownable {
     function withdraw(address _assetAddress) public onlyOwner {
         uint assetBalance;
         if (_assetAddress == ETHER) {
-            address self = address(this); // workaround for a possible solidity bug
-            assetBalance = self.balance;
-            msg.sender.transfer(assetBalance);
+            assetBalance = address(this).balance;
+            payable(msg.sender).transfer(assetBalance);
         } else {
             assetBalance = ERC20(_assetAddress).balanceOf(address(this));
             ERC20(_assetAddress).safeTransfer(msg.sender, assetBalance);
